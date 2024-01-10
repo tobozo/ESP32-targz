23353c0135cf304473b3cdde24b2a2ce56e16bfd
tobozo <tobozo@users.noreply.github.com>
2024-01-10 16:57:40 +0100

    added platformio CI tests

--- examples/Test_platformio/src/main.cpp
+++ examples/Test_platformio/src/main.cpp
@@ -1,8 +1,13 @@
+#define DEST_FS_USES_SD
+
 #include <ESP32-targz.h>
 
 void setup()
 {
-
+  if( !tarGzFS.begin() ) {
+    Serial.println("Could not start filesystem");
+    while(1) yield();
+  }
 }
 
 
