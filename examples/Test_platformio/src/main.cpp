2da2a5a49ff2fc56a3ce1c1e92e491cc5a25d0d8
tobozo <tobozo@users.noreply.github.com>
2026-01-29 20:16:40 +0100

    Simplified test

--- examples/Test_platformio/src/main.cpp
+++ examples/Test_platformio/src/main.cpp
@@ -1,14 +1,8 @@
-#define DEST_FS_USES_SD
-
 #include <ESP32-targz.h>
 
 void setup()
 {
   Serial.begin(115200);
-  if( !tarGzFS.begin() ) {
-    Serial.println("Could not start filesystem");
-    while(1) yield();
-  }
 
   TarUnpacker *TARUnpacker     = new TarUnpacker();
   GzUnpacker *GZUnpacker       = new GzUnpacker();
